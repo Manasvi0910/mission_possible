121657b2f6fb632281e7f40dd2d6754d2c17b37b
Manasvi0910 <97155655+Manasvi0910@users.noreply.github.com>
2023-06-04 20:49:43 +0530

    fourth commit

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ require("dotenv").config({path: "./config/config.env"});
 const express = require('express')
 const morgan = require("morgan");
 const connectDB = require("./config/db");
-
+const cors = require("cors");
 const auth = require("./middlewares/auth");
 
 const app = express();
 
+const corsOptions = {
+    origin: "https://contactify.onrender.com", // frontend URI (ReactJS)
+}
 //middlewares
 app.use(express.json());
 app.use(morgan("tiny"));
